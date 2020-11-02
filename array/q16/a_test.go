2a59287d8856779476d6bfc7ecec8e31a3b198e3
forjoin92 <forjoin92@gmail.com>
2020-11-02 16:27:36 +0800

    两个数组的交集

---
+++ array/q16/a_test.go
@@ -0,0 +1,21 @@
+package q16
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_intersection(t *testing.T) {
+	nums1 := []int{1, 2, 2, 1}
+	nums2 := []int{2, 2}
+	expected1 := []int{2}
+	actual1 := intersection(nums1, nums2)
+	assert.Equal(t, expected1, actual1)
+
+	nums3 := []int{4, 9, 5}
+	nums4 := []int{9, 4, 9, 8, 4}
+	expected2 := []int{9, 4}
+	actual2 := intersection(nums3, nums4)
+	assert.Equal(t, expected2, actual2)
+}
