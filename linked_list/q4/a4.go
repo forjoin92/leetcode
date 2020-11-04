3031452e6bd5c01d612711c80c3dc3a8076187cc
forjoin92 <forjoin92@gmail.com>
2020-11-04 16:21:20 +0800

    Update a4.go

--- linked_list/q4/a4.go
+++ linked_list/q4/a4.go
@@ -12,60 +12,39 @@ func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
 	if l2 == nil {
 		return l1
 	}
-	var res, tail *ListNode
-	if l1.Val < l2.Val {
-		res = &ListNode{
-			Val: l1.Val,
-		}
-		l1 = l1.Next
-		tail = res
-	} else if l2.Val < l1.Val {
-		res = &ListNode{
-			Val: l2.Val,
-		}
-		l2 = l2.Next
-		tail = res
-	} else {
-		res = &ListNode{
-			Val: l1.Val,
-			Next: &ListNode{
-				Val: l2.Val,
-			},
-		}
-		l1 = l1.Next
-		l2 = l2.Next
-		tail = res.Next
-	}
-	for l1 != nil && l2 != nil {
-		if l1.Val < l2.Val {
-			tail.Next = &ListNode{
-				Val: l1.Val,
-			}
+	var head, tail *ListNode
+	for l1 != nil || l2 != nil {
+		var n int
+		if l1 != nil && l2 == nil {
+			n = l1.Val
 			l1 = l1.Next
-			tail = tail.Next
-		} else if l2.Val < l1.Val {
-			tail.Next = &ListNode{
-				Val: l2.Val,
-			}
+		}
+		if l1 == nil && l2 != nil {
+			n = l2.Val
 			l2 = l2.Next
-			tail = tail.Next
+		}
+		if l1 != nil && l2 != nil {
+			if l1.Val < l2.Val {
+				n = l1.Val
+				l1 = l1.Next
+			} else {
+				n = l2.Val
+				l2 = l2.Next
+			}
+		}
+		if head == nil {
+			tail = &ListNode{
+				Val:  n,
+				Next: nil,
+			}
+			head = tail
 		} else {
 			tail.Next = &ListNode{
-				Val: l1.Val,
-				Next: &ListNode{
-					Val: l2.Val,
-				},
+				Val:  n,
+				Next: nil,
 			}
-			l1 = l1.Next
-			l2 = l2.Next
-			tail = tail.Next.Next
+			tail = tail.Next
 		}
 	}
-	if l1 != nil {
-		tail.Next = l1
-	}
-	if l2 != nil {
-		tail.Next = l2
-	}
-	return res
+	return head
 }
