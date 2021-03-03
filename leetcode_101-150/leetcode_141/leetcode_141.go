49416fd887b74a27cf6881dd4c169fcf0c6f1f2b
forjoin92 <forjoin92@gmail.com>
2021-03-03 23:48:32 +0800

    Update leetcode_141.go

--- leetcode_101-150/leetcode_141/leetcode_141.go
+++ leetcode_101-150/leetcode_141/leetcode_141.go
@@ -6,19 +6,12 @@ type ListNode struct {
 }
 
 func hasCycle(head *ListNode) bool {
-	if head == nil || head.Next == nil {
-		return false
-	}
-	fast, slow := head.Next, head
-	for fast != nil {
-		if fast.Next == nil || fast.Next.Next == nil {
-			return false
-		} else {
-			fast = fast.Next.Next
-			slow = slow.Next
-			if fast == slow {
-				return true
-			}
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
 		}
 	}
 	return false
